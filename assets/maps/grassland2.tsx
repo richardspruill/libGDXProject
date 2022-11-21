c0bc6763a071ec3ec48e4b389e9a5b18e2005b7c
Anthony <r.anthony.spruill@gmail.com>
2022-11-21 20:50:51 +0100

    added map and camera movement

---
+++ assets/maps/grassland2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="grassland2" tilewidth="64" tileheight="32" tilecount="512" columns="16">
+ <image source="../gassland_tiled/grassland.png" width="1024" height="1024"/>
+</tileset>
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,6 +7,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -16,6 +20,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Sprite grasslandSprite;
 	private OrthographicCamera camera;
 	private Rectangle character;
+	private TiledMap map1;
+	private MapRenderer mapRenderer;
 	SpriteBatch batch;
 	Texture img;
 	
@@ -26,25 +32,30 @@ public class MyGdxGame extends ApplicationAdapter {
 		grasslands.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
 		grasslandSprite = new Sprite(grasslands);
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 400);
+		camera.setToOrtho(false, 400, 200);
 		maleBack = new Texture("male/male_back.png");
 		img = new Texture("badlogic.jpg");
 		character = new Rectangle();
-		character.x = 800;
-		character.y = 20;
-		character.width = 64;
-		character.height = 64;
+		map1 = new TmxMapLoader().load("maps/map1.tmx");
+
+		float unitScale = 1/4f;
+		mapRenderer = new IsometricTiledMapRenderer(map1, unitScale);
+		character.x = 1;
+		character.y = 1;
+		character.width = 8;
+		character.height = 8;
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0.5f, 1);
-		batch.setProjectionMatrix(camera.combined);
+		mapRenderer.setView(camera);
+		mapRenderer.render();
+//		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-			grasslandSprite.draw(batch);
+//		grasslandSprite.draw(batch);
 		batch.draw(maleBack, character.x, character.y);
 		batch.end();
-		camera.update();
 
 
 		//Character movement
@@ -63,6 +74,9 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		if(character.x < 0) character.x = 0;
 		if(character.x > 800 - 64) character.x = 800 - 64;
+
+		 camera.position.set(character.getX(), character.getY(), 0);
+		 camera.update();
 	}
 	
 	@Override
